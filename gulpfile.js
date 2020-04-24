ee7ead08e8518d8e19baa2a2b66502f9cd0724c5
Mathilde Arconte <signup@marconte.fr>
2020-04-24 16:34:43 +0200

    vars fix

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@
 /* SETTINGS
 /===================================================== */
 // local domain used by browsersync
-var browsersync_proxy = "localhost";
+var browsersync_proxy = "localhost:10010";
 
 // default asset paths
 var assets = {
