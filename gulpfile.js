9217b0d183aa959ae62c4af45e302cdd7e7c5b81
Mathilde Arconte <signup@marconte.fr>
2020-04-24 13:32:35 +0200

    vars fix: gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -220,12 +220,11 @@ gulp.task('build-delete', function() {
   return del(['dist/**/*', '!dist/silence.zip']);
 });
 
-gulp.task('build',
-  gulp.series('default','build-clean', 'build-copy', 'build-zip', 'build-delete')
-);
-
-
 /* DEFAULT
 /––––––––––––––––––––––––*/
 // default gulp tasks executed with `gulp`
 gulp.task('default', gulp.series('css', 'cachebust', 'javascript','makepot'));
+
+gulp.task('build',
+  gulp.series('default','build-clean', 'build-copy', 'build-zip', 'build-delete')
+);
