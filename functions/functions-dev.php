9d4f044d78c1759590ae589e6f827735e1fc9f18
Mathilde Arconte <signup@marconte.fr>
2020-04-24 12:56:21 +0200

    fix gtm

--- functions/functions-dev.php
+++ functions/functions-dev.php
@@ -116,7 +116,7 @@ function slugify($text) {
 /––––––––––––––––––––––––*/
 // outputs one of the two parts of the Google Tag Manager scripts
 // Usage: gtm('head', 'GTM-1234567) and gtm('body', 'GTM-1234567)
-function _s_gtm($type) {
+function silence_gtm($type) {
   global $GTM_id;
   if ($GTM_id) :
     if ($type == 'head') : ?>
