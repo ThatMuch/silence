040ea214e12bb2b5798bd4daff76940c2a9bf36f
Mathilde Arconte <signup@marconte.fr>
2020-05-01 11:27:50 +0200

    slider :heavy_plus_sign: install slick

--- gulpfile.js
+++ gulpfile.js
@@ -160,7 +160,7 @@ gulp.task('javascript', gulp.series('clean:javascript', function() {
       'assets/scripts/modernizr.js',
       'assets/scripts/*.js'
     ], { base: './' }))
-    .pipe(plumber({errorHandler: notify.onError("Error")}))
+    .pipe(plumber({errorHandler: notify.onError("<%= error.message %>")}))
     .pipe(concat('script.min.js'))
     .pipe(uglify())
     .pipe(rename('./script.min.js'))
