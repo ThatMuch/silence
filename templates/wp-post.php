65eb655a5313d38d98fcea902f0d99e0575808c6
Mathilde Arconte <signup@marconte.fr>
2020-05-25 14:12:53 +0200

    js fix blog

--- templates/wp-post.php
+++ templates/wp-post.php
@@ -5,10 +5,9 @@
 			<?php the_post_thumbnail('large', ['class' => 'modernizr-of']); ?>
 			</div>
 		<?php endif; ?>
-		<div class=" article_content <?php if (has_post_thumbnail($post->ID)): ?> col-sm-6 <?php else : ?> col-sm-12<?endif; ?>">
+		<div class=" article_content <?php if (has_post_thumbnail($post->ID)): ?> col-sm-6 <?php else : ?> col-sm-12<?php endif; ?>">
 			<h2 class="entry-title"><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h2>
 			<?php the_excerpt(); ?>
-
 			<a class="btn btn-outline-primary" href=" <?php the_permalink() ?>"> Lire </a>
 		</div>
 	</div>
