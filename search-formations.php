c04b38ad0083c2e88a44884560d2464c5564d01e
Mathilde Arconte <signup@marconte.fr>
2020-06-15 12:33:24 +0200

    fix links cards search

--- search-formations.php
+++ search-formations.php
@@ -64,8 +64,10 @@
                                                <?php if ( get_field('desc') ) : ?>
                                                    <div class="card__back__text"><?php echo get_field('desc'); ?></div>
                                                <?php endif; ?>
-                                               <a class="btn btn-primary mt-auto btn-icon">Nous contacter<i class="icon arrow-right"></i></a>
-                                               <a class="btn btn-outline-primary btn-icon">Télécharger la brochure<i class="icon download"></i></a>
+                                               <a href="<?php echo site_url()?>/contact" class="btn btn-primary mt-auto btn-icon mb-2">Nous contacter<i class="icon arrow-right"></i></a>
+                                               <?php if ( get_field('brochure') ) : ?>
+											 <a href="<?php  the_field('brochure'); ?>" target="_blank" class="btn btn-outline-primary btn-icon">Télécharger la brochure<i class="icon download"></i></a>
+											<?php endif; ?>
                                            </div>
                                        </div>
 
