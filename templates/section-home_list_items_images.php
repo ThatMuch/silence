8b031f943fe638c9b2bc9af413be8c9a45f7cf43
Mathilde Arconte <signup@marconte.fr>
2021-11-05 15:47:24 +0100

    testimonials

--- templates/section-home_list_items_images.php
+++ templates/section-home_list_items_images.php
@@ -1,20 +1,23 @@
 <section class="section section__list_items_image">
-<!-- Header -->
-	<?php include get_template_directory().'/components/section_header-line.php' ?>
-<!-- Header -->
-	<?php if ( have_rows( 'list_items' ) ) : ?>
-		<div class="grid">
-			<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
-				<?php $img = get_sub_field( 'img' ); ?>
-				<div class="item">
-					<?php if ( $img ) : ?>
-						<img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" />
-					<?php endif; ?>
-					<?php the_sub_field( 'txt' ); ?>
+	<div class="container">
+		<!-- Header -->
+			<?php include get_template_directory().'/components/section_header-line.php' ?>
+		<!-- Header -->
+			<?php if ( have_rows( 'list_items' ) ) : ?>
+				<div class="grid">
+					<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
+						<?php $img = get_sub_field( 'img' ); ?>
+						<div class="item">
+							<?php if ( $img ) : ?>
+								<img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" />
+							<?php endif; ?>
+							<?php the_sub_field( 'txt' ); ?>
+						</div>
+					<?php endwhile; ?>
+				<?php else : ?>
+					Vide
 				</div>
-			<?php endwhile; ?>
-		<?php else : ?>
-			Vide
-		</div>
-			<?php endif; ?>
+					<?php endif; ?>
+
+	</div>
 </section>
