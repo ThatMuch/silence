3c7aae0772f45ac7c097162fb0aac35ef7f6bf60
Mathilde Arconte <signup@marconte.fr>
2021-12-14 17:06:20 +0100

    list items

--- templates/section-home_list_items.php
+++ templates/section-home_list_items.php
@@ -1,28 +1,41 @@
-<section class="section section__list_items">
+<section class="section__area border-bottom">
 	<div class="container">
-		<!-- Header Section-->
-		<?php include get_template_directory() . '/components/section_header-dotted.php'?>
-    	<!-- Header Section -->
-		<?php if ( get_sub_field('txt') ) : ?>
-			<?php echo get_sub_field('txt'); ?>
-		<?php endif; ?>
-		<div class="grid">
-			<?php if ( have_rows( 'list_items' ) ) : ?>
-					<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
-					<div class="item">
-						<?php $img = get_sub_field( 'img' ); ?>
-						<?php if ( $img ) : ?>
-							<img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" />
-						<?php endif; ?>
-						<?php the_sub_field( 'txt' ); ?>
-					</div>
-					<?php endwhile; ?>
-				<?php else : ?>
-					<?php // no rows found ?>
-				<?php endif; ?>
+		<div class="row text-center">
+			<div class="col-lg-12">
+				<h2 class="m-title"><?php echo get_sub_field('title'); ?></h2>
+			</div>
+			<div class="col-md-12 mt-100 mb-4">
+				<h2 class="title">Vous avez besoin de :</h2>
+			</div>
+        </div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<div class="service__list">
+					<?php if ( have_rows( 'list_items' ) ) : ?>
+						<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
+							<div class="service__item">
+								<div class="service__icon">
+									<div>
+										<?php $img = get_sub_field( 'img' ); ?>
+										<?php if ( $img ) : ?>
+											<img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" />
+										<?php endif; ?>
+									</div>
+								</div>
+								<p class="service__title">
+									<?php the_sub_field( 'txt' ); ?>
+								</p>
+							</div>
+						<?php endwhile; ?>
+					<?php endif; ?>
+				</div>
+			</div>
 		</div>
-
-		<?php the_sub_field( 'txt_2' ); ?>
-
+		<div class="row mt-100">
+			<div class="col-md-12 text-center">
+				<h2 class="m-title"><?php the_sub_field( 'txt_2' ); ?></h2>
+			</div>
+		</div>
 	</div>
 </section>
