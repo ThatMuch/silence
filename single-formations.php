f5d91ef7cd6df5ac209123d9162f2a847620c1bf
Mathilde Arconte <signup@marconte.fr>
2021-12-28 15:17:07 +0100

    page formation

--- single-formations.php
+++ single-formations.php
@@ -12,211 +12,130 @@
 get_header();
 ?>
 
-        <div class="section__area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="text-center m-title">Bienvenue au cercle des oratrices !</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!-- H2 -->
+<div class="section__area">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<?php if ( get_field('subtitle_page') ) : ?>
+					<h2 class="text-center m-title"><?php echo esc_html_e(get_field('subtitle_page')); ?></h2>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
 
-        <div class="section__area bg-green">
-            <div class="container">
-                <div class="row gx-5">
-                    <div class="col-lg-4">
-                        <img class="round-image" src="assets/img/image-21.jpg" alt="">
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="text">
-                            <h2 class="m-title text-light mb-4">Dates</h2>
-                            <p><strong>Du lundi 30 août 2021</strong> au <strong>lundi 18 octobre 2021</strong>, de 14h à 16h pour 8 rencontres. <br>30/08, 06/09, 13/09, 20/09, 27/09, 04/10, 11/10, 18/10 COMPLET !</p>
-                            <p><strong>Du lundi 30 août 2021</strong> au lundi <strong>18 octobre 2021</strong>, de 18h30 à 20h30 pour 8 rencontres. <br>30/08, 06/09, 13/09, 20/09, 27/09, 04/10, 11/10, 18/10 COMPLET !</p>
-                            <p><strong>Du mardi 21 septembre 2021</strong> au <strong>mardi 16 novembre 2021</strong>, de 18h30 à 20h30 pour 8 rencontres. Avec une pause d'1 semaine à la Toussaint. <br>21/09, 28/09, 05/10, 12/10, 19/10, 26/10, 09/11, 16/11</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!-- Section 1 -->
+<?php if ( have_rows( 'formation_section_1' ) ) : ?>
+	<?php while ( have_rows( 'formation_section_1' ) ) : the_row(); ?>
+		<div class="section__area bg-green">
+			<div class="container">
+				<div class="row gx-5">
+					<div class="col-lg-4">
+						<?php $image = get_sub_field( 'img' ); ?>
+						<?php if ( $image  ) : ?>
+							<img class="round-image" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] );?>">
+						<?php endif; ?>
+					</div>
+					<div class="col-lg-8">
+						<div class="text">
+							<?php if ( get_sub_field('title') ) : ?>
+								<h2 class="m-title text-light mb-4"><?php echo esc_html_e(get_sub_field('title')); ?></h2>
+							<?php endif; ?>
+							<?php if ( get_sub_field('text') ) : ?>
+								<?php echo get_sub_field('text'); ?>
+							<?php endif; ?>
+								<?php $link = get_sub_field( 'link' ); ?>
+							<?php if ( $link ) : ?>
+			<a class="btn btn-white link m-full" href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>"><?php echo esc_html( $link['title'] ); ?></a>
+		<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endwhile; ?>
+<?php endif; ?>
 
-        <div class="section__area">
-            <div class="container">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-4 text-center order-lg-1">
-                        <img class="round-image-half mb-5 mb-lg-0" src="assets/img/image-22.jpg" alt="">
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="text-bx fw-medium">
-                            <h2 class="m-title text-center text-md-start mb-4 mb-lg-0">Réservez votre place !</h2>
-                            <p>En remplissant ce formulaire, votre sera définitivement réservée pour le cercle de formation. <br>Nous vous contacterons dans les prochaines 48h pour la suite :</p>
-                            <ul>
-                                <li>dates & horaires de formation </li>
-                                <li>signature d’une convention de formation </li>
-                                <li>paiement ou mise en place des actions pour faire financer votre formation par votre OPCO, CPF ou Pôle Emploi</li>
-                                <li>Nos tarifs sont : 600€ TTC pour un paiement en tant que Particulier ; 900€ TTC pour un paiement en tant qu'entreprise, CPF, OPCO, AIF Pôle Emploi.</li>
-                            </ul>
-                            <p>Vous pouvez également vous rendre directement sur votre Compte Personnel de Formation pour faire financer la formation dans la ville de votre choix.</p>
-                            <a href="#" class="btn btn-green link m-full">mon compte cpf</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!-- Section 2 -->
+<?php if ( have_rows( 'formation_section_2' ) ) : ?>
+	<?php while ( have_rows( 'formation_section_2' ) ) : the_row(); ?>
+		<div class="section__area">
+			<div class="container">
+				<div class="row gx-5 align-items-center">
+					<div class="col-lg-4 text-center order-lg-1">
+						<?php $img = get_sub_field( 'img' ); ?>
+						<?php if ( $img ) : ?>
+			<img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" class="round-image-half mb-5 mb-lg-0"/>
+		<?php endif; ?>
 
-        <div class="section__area border-top-white border-bottom-white">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-10 mx-auto">
-                        <h2 class="title">Rejoignez le prochain cercle</h2>
-                        <div class="contact__form">
-                            <div class="row gx-4 justify-content-center">
-                                <div class="col-md-12">
-                                    <h2 class="text-center">civilité</h2>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">nom</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">prénom</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">entreprise</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row gx-4 justify-content-center">
-                                <div class="col-md-12">
-                                    <h2 class="text-center">contact</h2>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">email</label>
-                                        <input type="email">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">téléphone</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row gx-4 justify-content-center">
-                                <div class="col-md-12">
-                                    <h2 class="text-center">adresse</h2>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">adresse</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">ville</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="contact__input">
-                                        <label class="bg-white">code postale</label>
-                                        <input type="text">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row gx-4 justify-content-center">
-                                <div class="col-md-12">
-                                    <h2 class="text-center">mes objectifs</h2>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="contact__input">
-                                        <label class="bg-white">je souhaite</label>
-                                        <select name="" id="">
-                                            <option value="">Google</option>
-                                            <option value="">Facebook</option>
-                                            <option value="">Online</option>
-                                        </select>
-                                    </div>
-                                    <div class="contact__input">
-                                        <label class="bg-white">formation choisie</label>
-                                        <select name="" id="">
-                                            <option value="">Google</option>
-                                            <option value="">Facebook</option>
-                                            <option value="">Online</option>
-                                        </select>
-                                    </div>
-                                    <div class="contact__input">
-                                        <label class="bg-white">paiement</label>
-                                        <select name="" id="">
-                                            <option value="">Google</option>
-                                            <option value="">Facebook</option>
-                                            <option value="">Online</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="contact__input">
-                                        <label class="bg-white">complément d’informations</label>
-                                        <textarea cols="30" rows="11"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row align-items-center">
-                                <div class="col-md-9">
-                                    <div class="custom-check">
-                                        <input type="checkbox" id="checkbox-1" checked>
-                                        <label for="checkbox-1">S’inscrire à la newsletter</label>
-                                    </div>
-                                    <div class="custom-check">
-                                        <input type="checkbox" id="checkbox-2">
-                                        <label for="checkbox-2">J’accepte les politiques de confidentialite</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-3 text-end">
-                                    <button class="btn btn-green link">envoyer</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+					</div>
+					<div class="col-lg-8">
+						<div class="text-bx fw-medium">
+							<h2 class="m-title text-center text-md-start mb-4 mb-lg-0">	<?php the_sub_field( 'title' ); ?></h2>
+							<?php the_sub_field( 'text' ); ?>
+								<?php $link = get_sub_field( 'link' ); ?>
+								<?php if ( $link ) : ?>
+			<a class="btn btn-green link m-full" href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>"><?php echo esc_html( $link['title'] ); ?></a>
+		<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endwhile; ?>
+<?php endif; ?>
 
-        <div class="section__area bg-gray">
-            <div class="container">
-                <div class="row gx-5">
-                    <div class="col-lg-4 text-center">
-                        <img class="round-image-half border-green" src="assets/img/image-23.jpg" alt="">
-                    </div>
-                    <div class="col-lg-7">
-                        <div class="text fw-medium">
-                            <ul>
-                                <li>Caractériser le public cible de chaque intervention pour répondre à ses besoins et réussir à parler AU public. </li>
-                                <li>Identifier votre message principal et développer l’esprit de synthèse à l’oral </li>
-                                <li>Préparer en autonomie vos prises de parole pour être à l’aise </li>
-                                <li>Organiser ses idées et oser l’émotion dans ses prises de parole </li>
-                                <li>Accueillir ses émotions et gérer son stress avant et pendant sa prise de parole </li>
-                                <li>Mobiliser l’expression corporelle au travers du regard, de la posture, de la gestuelle et de la voix </li>
-                                <li>Apprendre à gérer son souffle et à respirer pour projeter sa voix et manifester de l’enthousiasme</li>
-                                <li>Savoir se mettre en valeur notamment en mobilisant l'outil « storytelling » pour convaincre et persuader</li>
-                                <li>Prendre le lead en réunion et face public, se sentir légitime pour faire entendre ses idées, dépasser les freins liés à la timidité ou à</li>
-                                <li>intériorisation de complexes ou de faiblesses</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!-- Contact -->
+<?php if ( have_rows( 'formation_form_group' ) ) : ?>
+	<?php while ( have_rows( 'formation_form_group' ) ) : the_row(); ?>
+		<div class="section__area border-top-white border-bottom-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-10 mx-auto">
+						<h2 class="title"><?php the_sub_field( 'title' ); ?></h2>
+						<div class="contact__form">
+						<?php $form = get_sub_field('formation_form');?>
+						<?php if($form) : ?>
+								<?php echo $form; ?>
+						<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endwhile; ?>
+<?php endif; ?>
 
+<!-- Section 3 -->
+<?php if ( have_rows( 'formation_section_3' ) ) : ?>
+	<?php while ( have_rows( 'formation_section_3' ) ) : the_row(); ?>
+		<div class="section__area">
+			<div class="container">
+				<div class="row gx-5 align-items-center" style="    flex-direction: row-reverse;">
+					<div class="col-lg-4 text-center order-lg-1">
+						<?php $img = get_sub_field( 'image' ); ?>
+						<?php if ( $img ) : ?>
+							<img src="<?php echo esc_url( $img['url'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" class="round-image-half mb-5 mb-lg-0"/>
+						<?php endif; ?>
+					</div>
+					<div class="col-lg-8">
+						<div class="text-bx fw-medium">
+							<h2 class="m-title text-center text-md-start mb-4 mb-lg-0">	<?php the_sub_field( 'title' ); ?></h2>
+							<?php the_sub_field( 'text' ); ?>
+								<?php $link = get_sub_field( 'link' ); ?>
+								<?php if ( $link ) : ?>
+									<a class="btn btn-green link m-full" href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>"><?php echo esc_html( $link['title'] ); ?></a>
+								<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endwhile; ?>
+<?php endif; ?>
+
+<!-- Actualites -->
  <section class="section__area bg-yellow">
 	<div class="container">
                   <div class="row">
@@ -246,3 +165,4 @@ get_header();
             </div>
       </div>
  </section>
+ <?php get_footer(); ?>
