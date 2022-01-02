18efe3dd449c71968f0477363e88481ab81cd248
Mathilde Arconte <signup@marconte.fr>
2022-01-02 21:31:50 +0100

    build

--- templates/section-formations.php
+++ templates/section-formations.php
@@ -54,40 +54,11 @@
 								<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 								<img src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?>">
 							</div>
-<<<<<<< HEAD
-				<?php
-						endwhile;
-					else: ?>
-					<p class="text-center w-100">Aucune formation</p>
-					<?php endif;
-					wp_reset_query();
-				?>
-				</div>
-				</div>
-			<?php endforeach; ?>
-			<div class="tab-pane fade " id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-			<div class="col-sm-4 d-flex justify-content-center">
-			<div class="card-training card">
-									<div class="card__inner">
-										<div class="card__front">
-											<div class="img__inner"><img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title()?>" /></div>
-
-											<div class="content-center-verticale">
-												<h3 class="card__title">Nos conseils sur mesure</h3>
-											</div>
-										</div>
-										<div class="card__back ">
-											<h3 class="card__back__title">Nos conseils sur mesure</h3>
-
-												<p class="card__back__text">Les séances de coaching individuel “Gagner confiance à l’oral” sont destinées aux personnes souhaitant mieux gérer leurs peurs, leurs appréhensions, leurs émotions et leur stress à l’oral. Vous acquérez ainsi un solide socle d’outils techniques pour réussir vos prises de parole.
-Vous déterminez le nombre de séances nécessaires et choisissez les thèmes de chacune de vos séances de coaching parmi la liste proposée dans le document à télécharger.</p>
-=======
 							<div class="item__text">
 								<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 							</div>
 						</div>
 					</div>
->>>>>>> release/v.2.0.0
 
 					<?php }} wp_reset_postdata(); ?>
 				</div>
