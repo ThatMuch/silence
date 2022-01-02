c6686669a3ecb64261af5dfc034dff183369f3a1
Mathilde Arconte <signup@marconte.fr>
2022-01-02 20:16:26 +0100

    entreprise

--- entreprises.php
+++ entreprises.php
@@ -18,7 +18,7 @@ get_header();
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
-                        <h2 class="text-center m-title">Formez-vous à la prise de parole pour faire face à toutes les situations </h2>
+                        <h2 class="text-center m-title"><?php echo esc_html_e(get_field('subtitle_page')); ?></h2>
                     </div>
                 </div>
             </div>
@@ -28,23 +28,14 @@ get_header();
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-3 text-center">
-                        <img class="round-image-half" src="assets/img/image-27.jpg" alt="">
+						<?php $company_section1_image = get_field( 'company_section1_image' ); ?>
+						<?php if ( $company_section1_image ) : ?>
+							<img class="round-image-half" src="<?php echo esc_url( $company_section1_image['url'] ); ?>" alt="<?php echo esc_attr( $company_section1_image['alt'] ); ?>" />
+						<?php endif; ?>
                     </div>
                     <div class="col-lg-8 ms-auto">
                         <div class="text-b">
-                            <p class="text-orange fw-light text-uppercase">vous aimerez :</p>
-                            <ul>
-                                <li>Savoir répondre de manière claire et sans stress en toutes circonstances.</li>
-                                <li>Gagner en efficacité en réunion, en Pitch, en RDV clients. </li>
-                                <li>Avoir un impact et ne pas se censurer. </li>
-                            </ul>
-                            <p class="text-orange fw-light text-uppercase">de nombreux aspects sont travaillés lors de nos formations :</p>
-                            <ul>
-                                <li>La confiance en soi, mais aussi en l’autre et avec ses pairs</li>
-                                <li>La connaissance de ses forces, de ses aptitudes </li>
-                                <li>La connaissance de sa capacité à être « puissant », efficace et se sentir légitime</li>
-                            </ul>
-                            <p>Grâce à ce travail, de nombreuses nouvelles idées peuvent émerger. De plus, c’est l’occasion de passer un moment convivial au bénéfice du collectif. En créant un cercle de formation interne à votre entreprise, vous répondrez directement à plusieurs enjeux stratégiques. </p>
+                            <?php the_field( 'company_section1_text' ); ?>
                         </div>
                     </div>
                 </div>
@@ -55,47 +46,23 @@ get_header();
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
-                        <h2 class="title mb-75">5 choix s’offrent à vous pour réaliser nos cercles de formation chez vous</h2>
+                        <h2 class="title mb-75"><?php the_field( 'company_choices_title' ); ?></h2>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
                         <div class="service__list">
-                            <div class="service__item seven">
+						<?php if ( have_rows( 'company_choices_item' ) ) : ?>
+							<?php while ( have_rows( 'company_choices_item' ) ) : the_row(); ?>
+							<div class="service__item seven">
                                 <div class="service__icon">
-                                    <h2><a href="#">progressive</a></h2>
+                                    <h2><?php the_sub_field( 'choice' ); ?></h2>
                                 </div>
-                                <h3>8 rencontres de 2 heures en groupe</h3>
-                                <p>à vous de choisir le jour et l’heure en fonction de ce qui vous arrange !</p>
+                                <h3><?php the_sub_field( 'title' ); ?></h3>
+                                <?php the_sub_field( 'text' ); ?>
                             </div>
-                            <div class="service__item seven">
-                                <div class="service__icon">
-                                    <h2><a href="#">sereine</a></h2>
-                                </div>
-                                <h3>4 demi-journées</h3>
-                                <p>Espacées d’1 à 2 semaines</p>
-                            </div>
-                            <div class="service__item eight">
-                                <div class="service__icon">
-                                    <h2><a href="#">intensive</a></h2>
-                                </div>
-                                <h3>2 journées consécutives</h3>
-                                <p>Progression rapide garantie à condition d’être prêt !</p>
-                            </div>
-                            <div class="service__item nine">
-                                <div class="service__icon">
-                                    <h2><a href="#">Hybride</a></h2>
-                                </div>
-                                <h3>E-learning ; Coaching individuel et atelier de formation en groupe</h3>
-                                <p>la formation sur mesure pour faire face au travail à distance ! </p>
-                            </div>
-                            <div class="service__item nine">
-                                <div class="service__icon">
-                                    <h2><a href="#">sur-mesure</a></h2>
-                                </div>
-                                <h3>sur-mesure</h3>
-                                <p> la formation sur mesure pour faire face au travail à distance ! </p>
-                            </div>
+							<?php endwhile; ?>
+						<?php endif; ?>
                         </div>
                     </div>
                 </div>
@@ -106,17 +73,25 @@ get_header();
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-3 pl-lg-5 text-center order-lg-1">
-                        <img src="assets/img/image-16.png" alt="">
+                        <?php $company_section2_image = get_field( 'company_section2_image' ); ?>
+<?php if ( $company_section2_image ) : ?>
+	<img src="<?php echo esc_url( $company_section2_image['url'] ); ?>" alt="<?php echo esc_attr( $company_section2_image['alt'] ); ?>" />
+<?php endif; ?>
                     </div>
                     <div class="col-lg-6">
                         <div class="text-bs">
-                            <h5 class="fw-bold text-orange text-uppercase mb-30">tarifs et financement</h5>
-                            <p class="mb-30">Pour toute <a href="#"><strong>demande de devis</strong></a> contactez-nous via notre formulaire </p>
-                            <h5 class="fw-bold text-orange text-uppercase mb-30">covid 19</h5>
-                            <p>Pour toutes vos questions liées à l’adaptation aux conditions sanitaires, consultez notre <a href="#"><strong>FAQ</strong></a></p>
+                            <h5 class="fw-bold text-orange text-uppercase mb-30"><?php the_field( 'company_section2_title' ); ?></h5>
+                            <?php the_field( 'company_section2_text' ); ?>
                             <div class="btn-list justify-content-center mt-5">
-                                <a href="#" class="btn btn-green link">demande de devis</a>
-                                <a href="#" class="btn btn-border orange bg-white link">faq</a>
+								<?php $company_section2_link1 = get_field( 'company_section2_link1' ); ?>
+<?php if ( $company_section2_link1 ) : ?>
+	<a class="btn btn-green link" href="<?php echo esc_url( $company_section2_link1['url'] ); ?>" target="<?php echo esc_attr( $company_section2_link1['target'] ); ?>"><?php echo esc_html( $company_section2_link1['title'] ); ?></a>
+<?php endif; ?>
+
+<?php $company_section2_link2 = get_field( 'company_section2_link2' ); ?>
+<?php if ( $company_section2_link2 ) : ?>
+	<a class="btn btn-border orange bg-white link" href="<?php echo esc_url( $company_section2_link2['url'] ); ?>" target="<?php echo esc_attr( $company_section2_link2['target'] ); ?>"><?php echo esc_html( $company_section2_link2['title'] ); ?></a>
+<?php endif; ?>
                             </div>
                         </div>
                     </div>
@@ -128,18 +103,20 @@ get_header();
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h2 class="title mb-75">Où se déroule la formation ?</h2>
+                        <h2 class="title mb-75"><?php the_field( 'company_section3_title' ); ?></h2>
                     </div>
                 </div>
                 <div class="row gx-5">
                     <div class="col-lg-3">
-                        <img class="round-image" src="assets/img/image-28.jpg" alt="">
+						<?php $company_section3_image = get_field( 'company_section3_image' ); ?>
+<?php if ( $company_section3_image ) : ?>
+	<img  class="round-image" src="<?php echo esc_url( $company_section3_image['url'] ); ?>" alt="<?php echo esc_attr( $company_section3_image['alt'] ); ?>" />
+<?php endif; ?>
+
                     </div>
                     <div class="col-lg-9">
                         <div class="text-bx">
-                            <p>Si vous préférez être formés dans vos locaux, il suffit juste qu’il soit conforme aux besoins de formations (surface, écran/vidéo-projecteur, confidentialité respectée, accès PMR si besoin). La formation peut aussi se dérouler dans nos locaux ou alors dans un lieu choisi ensemble. </p>
-                            <p>Il est important pour nous que vous vous sentiez bien afin que l’apprentissage se déroule dans un cadre confortable et agréable. </p>
-                            <p>Comme pour le lieu, nous sommes très flexibles sur les formations. Nous pouvons les adapter et faire du sur-mesure, ou alors un contenu standard. Tout dépend de vous ! Nos formateurs sont à l’écoute et présents pour accompagner vos équipes selon leurs compétences et leurs envies. </p>
+                            <?php the_field( 'company_section3_text' ); ?>
                         </div>
                     </div>
                 </div>
@@ -150,88 +127,31 @@ get_header();
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-3 pl-lg-5 text-center order-lg-1">
-                        <img src="assets/img/image-16.png" alt="">
+                        <?php $company_section4_image = get_field( 'company_section4_image' ); ?>
+<?php if ( $company_section4_image ) : ?>
+	<img src="<?php echo esc_url( $company_section4_image['url'] ); ?>" alt="<?php echo esc_attr( $company_section4_image['alt'] ); ?>" />
+<?php endif; ?>
                     </div>
                     <div class="col-lg-7">
                         <div class="text-bb">
-                            <p>Tous nos cercles de formation sont aussi disponibles en <strong>formation individuelle</strong>, en <strong>présentiel</strong> ou en <strong>distanciel</strong> avec un coach. </p>
-                            <p>Si c’est ce que vous recherchez, <strong>contactez-nous</strong> pour adapter la formation à vos besoins ! </p>
+							<?php if (get_field('company_section4_title')) : ?>
+							<h5 class="fw-bold text-orange text-uppercase mb-30">	<?php the_field( 'company_section4_title' ); ?> </h5>
+								<?php endif;?>
+                            <?php the_field( 'company_section4_text' ); ?>
                             <div class="btn-list justify-content-center mt-5">
-                                <a href="#" class="btn btn-green link">demande de devis</a>
-                                <a href="#" class="btn btn-border orange bg-white link">faq</a>
+								<?php $company_section4_link1 = get_field( 'company_section4_link1' ); ?>
+								<?php $company_section4_link2 = get_field( 'company_section4_link2' ); ?>
+								<?php if ( $company_section4_link1 ) : ?>
+									<a class="btn btn-green link" href="<?php echo esc_url( $company_section4_link1['url'] ); ?>" target="<?php echo esc_attr( $company_section4_link1['target'] ); ?>"><?php echo esc_html( $company_section4_link1['title'] ); ?></a>
+								<?php endif; ?>
+								<?php $company_section4_link2 = get_field( 'company_section4_link2' ); ?>
+								<?php if ( $company_section4_link2 ) : ?>
+									<a class="btn btn-border orange bg-white link" href="<?php echo esc_url( $company_section4_link2['url'] ); ?>" target="<?php echo esc_attr( $company_section4_link2['target'] ); ?>"><?php echo esc_html( $company_section4_link2['title'] ); ?></a>
+								<?php endif; ?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="footer__area bg-primary">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-7">
-                        <div class="footer__logo">
-                            <img src="assets/img/footer-logo.png" alt="">
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                                <p>SIRET : 6535185586</p>
-                                <p>organisme de formation : 65351855</p>
-                                <p>Contactez-nous</p>
-                                <a href="#" class="btn btn-orange"><img src="assets/img/phone.svg" alt=""> 0653518466</a>
-                            </div>
-                            <div class="col-md-8">
-                                <ul>
-                                    <li><a href="#">Formations</a></li>
-                                    <li><a href="#">Inscriptions</a></li>
-                                    <li><a href="#">Le concept</a></li>
-                                    <li><a href="#">Cercle des Oratrices</a></li>
-                                    <li><a href="#">Nos programmes</a></li>
-                                    <li><a href="#">Cercle des leaders de demain</a></li>
-                                    <li><a href="#">Formation entreprise</a></li>
-                                    <li><a href="#">Cercle a haute voix</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="#">Cercle des differents leaders</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-5">
-                        <div class="footer__widget">
-                            <h2>Nos labels qualités</h2>
-                            <div class="row">
-                                <div class="col-6 col-lg-4">
-                                    <div class="footer__widget__image">
-                                        <img src="assets/img/client-10.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-6 col-lg-4">
-                                    <div class="footer__widget__image">
-                                        <img src="assets/img/client-11.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="footer__widget">
-                            <h2>Ils financent nos formations</h2>
-                            <div class="row">
-                                <div class="col-6 col-lg-4">
-                                    <div class="footer__widget__image">
-                                        <img src="assets/img/client-12.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-6 col-lg-4">
-                                    <div class="footer__widget__image">
-                                        <img src="assets/img/client-13.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <p>Notre offre de formation à la prise de parole en public est certifiée QUALIOPI, DATADOCK et CPF.</p>
-                        <p>Adresse </p>
-                        <p>Adresse </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<?php get_footer(); ?>
