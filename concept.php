812fda13538951cd739a926eb020c8a791386cc4
Mathilde Arconte <signup@marconte.fr>
2022-01-23 16:59:08 +0100

    liens par formations

--- concept.php
+++ concept.php
@@ -43,8 +43,8 @@ $query = new WP_Query( $args );
 							<h2 class="title-3" style="color: <?php the_field( 'color' ); ?>"><?php the_title(); ?></h2>
 							<?php the_field( 'desc' ); ?>
 							<div class="btn-list g-3 mt-4">
-								<a href="#" class="btn btn-green link">je m’inscris</a>
-								<a href="#" class="btn btn-green link">rdv découverte gratuit</a>
+								<a href="<?php the_permalink(); ?>/#contact" class="btn btn-green link">je m’inscris</a>
+								<a href="<?php echo esc_url(site_url()); ?>/rdv-decouvertes" class="btn btn-green link">rdv découverte gratuit</a>
 								<a href="<?php the_permalink(); ?>" class="btn btn-border orange link">la formation</a>
 							</div>
 						</div>
--- header.php
+++ header.php
@@ -31,9 +31,9 @@
 <nav class="header__area navbar navbar-expand-lg">
   <div class="container">
     <!-- Navbar brand -->
-  <a class="navbar-brand" href="<?php esc_html_e(site_url()); ?>">
+  <a class="navbar-brand" href="<?php echo esc_url(site_url()); ?>">
   <?php if($image[0]) : ?>
-    <img src="<?php $image[0]?>" alt="<?php esc_html_e(get_bloginfo( 'name' )); ?>">
+    <img src="<?php echo esc_url($image[0]);?>" alt="<?php esc_html_e(get_bloginfo( 'name' )); ?>">
     <?php else: ?>
   <div class="logo" style="background-image: url('<?php echo esc_html_e(get_template_directory_uri());?>/assets/images/stanlee_logo_texte.png')"></div>
   <?php endif;?>
